6ead31d2ed364be681540525f1ec7211daeb45de
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2021-09-25 13:07:05 -0700

    working

--- pages/_app.js
+++ pages/_app.js
@@ -5,73 +5,81 @@ import queryString from "query-string";
 const App = ({ Component, pageProps }) => {
   const router = useRouter();
 
-  // useEffect(() => {
-  //   console.log("SETUP", router.query, router.asPath);
+  useEffect(() => {
+    console.log("SETUP", router.query, router.asPath);
 
-  //   const handleRouteChange = async (url) => {
-  //     const basePath = (url || "").split(/\?/)?.[0] || "";
-  //     const queryParams = (url || "").split(/\?/)?.[1] || "";
-  //     console.log("route change", { url, basePath, queryParams });
+    // options includes a property called shallow
+    const handleRouteChange = async (url) => {
+      const basePath = (url || "").split(/\?/)?.[0] || "";
+      const queryParams = (url || "").split(/\?/)?.[1] || "";
+      console.log("route change", {
+        url,
+        basePath,
+        queryParams,
+      });
 
-  //     if (!basePath) return;
+      if (!basePath) return;
 
-  //     const query = queryString.parse(queryParams);
+      const query = queryString.parse(queryParams);
 
-  //     if (!query.code2test) {
-  //       const newUrl = `${basePath}?${queryString.stringify({
-  //         ...query,
-  //         code2test: "ali-here",
-  //       })}`;
+      if (!query.code2test) {
+        const newUrl = `${basePath}?${queryString.stringify({
+          ...query,
+          code2test: "ali-here",
+        })}`;
 
-  //       console.log("here", { url, basePath, queryParams, newUrl });
+        console.log("here", {
+          url,
+          basePath,
+          queryParams,
+          newUrl,
+        });
 
-  //       router.replace(newUrl, newUrl, { shallow: true });
+        router.replace(newUrl, newUrl);
+      }
 
-  //       throw "don't";
-  //     }
+      return true;
+    };
 
-  //     return true;
-  //   };
+    // on initial page load.
+    handleRouteChange(router?.asPath || "");
 
-  //   // on initial page load.
-  //   handleRouteChange(router?.asPath || "");
-
-  //   router.events.on("routeChangeStart", handleRouteChange);
-  //   return () => router.events.off("routeChangeStart", handleRouteChange);
-  // }, []);
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, []);
 
   return <Component {...pageProps} />;
 };
 
-App.getInitialProps = async ({ ctx, router }) => {
-  const url = ctx?.asPath || "";
-  const basePath = (url || "").split(/\?/)?.[0] || "";
-  const queryParams = (url || "").split(/\?/)?.[1] || "";
+// App.getInitialProps = async ({ ctx, router }) => {
+//   const url = ctx?.asPath || "";
+//   const basePath = (url || "").split(/\?/)?.[0] || "";
+//   const queryParams = (url || "").split(/\?/)?.[1] || "";
 
-  const query = queryString.parse(queryParams);
-  console.log("route change", { url, basePath, queryParams, query });
+//   const query = queryString.parse(queryParams);
+//   console.log("route change", { url, basePath, queryParams, query });
 
-  if (!query.code2test) {
-    const newUrl = `${basePath}?${queryString.stringify({
-      ...query,
-      code2test: "ali-here",
-    })}`;
+//   if (!query.code2test) {
+//     const newUrl = `${basePath}?${queryString.stringify({
+//       ...query,
+//       code2test: "ali-here",
+//     })}`;
 
-    // redirect
-    if (typeof window !== "undefined") {
-      // if in browser
-      router.replace(newUrl, newUrl);
-    } else if (ctx?.res && ctx?.res?.writeHead && ctx?.res?.end) {
-      // if ssr
-      ctx.res.writeHead(301, {
-        Location: newUrl,
-      });
+//     // redirect
+//     if (typeof window !== "undefined") {
+//       // if in browser
+//       router.replace(newUrl, newUrl);
+//     } else if (ctx?.res && ctx?.res?.writeHead && ctx?.res?.end) {
+//       // if ssr
+//       ctx.res.writeHead(301, {
+//         Location: newUrl,
+//       });
 
-      ctx.res.end();
-    }
-  }
+//       ctx.res.end();
+//     }
+//   }
 
-  return {};
-};
+//   return {};
+// };
 
 export default App;
